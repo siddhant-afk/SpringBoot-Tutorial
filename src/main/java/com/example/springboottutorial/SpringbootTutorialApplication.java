fb8c5d3c065df44cffa78fdd935d73b992deddf0
siddhant-afk <siddhantdudhane21@gmail.com>
2025-06-23 19:21:47 +0530

    Learnt to add first ever REST endpoint

--- src/main/java/com/example/springboottutorial/SpringbootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringbootTutorialApplication.java
@@ -2,12 +2,19 @@ package com.example.springboottutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootTutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTutorialApplication.class, args);
     }
+@GetMapping
+    public String hello(){
 
+        return "<h1 style=\"color : red\"> Hello World! </h1>";
+    }
 }
